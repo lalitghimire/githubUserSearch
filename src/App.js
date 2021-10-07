19c49974676be7d8fe2edb9970aaa5456270f676
Lalit Ghimire <lalitghimire@gmail.com>
2021-10-08 00:14:24 +0300

    working solution

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
+import axios from 'axios'
 import React, { useState } from 'react'
 
 const App = () => {
     const [searchName, setSearchName] = useState('')
+    const [repo, setRepo] = useState([])
 
     // event handler for button click
     const handleClick = () => {
-        console.log('button is clicked')
-        console.log(searchName)
+        // console.log('button clicked')
+        // console.log('search name is ', searchName)
+        axios
+            .get(`https://api.github.com/users/${searchName}/repos`)
+            .then((response) => {
+                setRepo(response)
+            })
+            .catch((error) => {
+                console.log('invalid username', error.message)
+            })
+    }
+    console.log('here is repodata', repo.data)
+    const displayInfo = () => {
+        if (repo.length === 0) {
+            return null
+        } else
+            return repo.data.map((repo) => (
+                <p key={repo.id}>
+                    {repo.name} {'  '}
+                    {repo.url}
+                </p>
+            ))
     }
 
     // event handler for search box
     const handleSearch = (e) => {
-        console.log(e.target.value)
+        // console.log(e.target.value)
         setSearchName(e.target.value)
     }
+
     return (
         <div>
             <h1>Github profile search</h1>
@@ -22,11 +45,7 @@ const App = () => {
                 <input placeholder='username..' onChange={handleSearch} />
                 <button onClick={handleClick}>search</button>
             </div>
-            <div>
-                <ul>
-                    <li> repo1</li>
-                </ul>
-            </div>
+            <div>{displayInfo()}</div>
         </div>
     )
 }

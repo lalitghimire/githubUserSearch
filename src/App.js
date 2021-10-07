92b8707b43bdff92e7e81666fceba04fe2a28584
Lalit Ghimire <lalitghimire@gmail.com>
2021-10-07 19:59:49 +0300

    added search box

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
 const App = () => {
-    return <div>Hello</div>
+    return (
+        <div>
+            <h1>Github profile search</h1>
+            Enter github username
+            <div>
+                <input placeholder='username..' />
+                <button>search</button>
+            </div>
+            <div>
+                <ul>
+                    <li> repo1</li>
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default App

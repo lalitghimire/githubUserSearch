62d9ddf23cc26b60f7f852879756da099413f61d
Lalit Ghimire <lalitghimire@gmail.com>
2021-10-07 20:10:16 +0300

    added event handlers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
 const App = () => {
+    // event handlers
+    const handleClick = () => {
+        console.log('button is clicked')
+    }
+
+    const handleSearch = (e) => {
+        console.log(e.target.value)
+    }
     return (
         <div>
             <h1>Github profile search</h1>
             Enter github username
             <div>
-                <input placeholder='username..' />
-                <button>search</button>
+                <input placeholder='username..' onChange={handleSearch} />
+                <button onClick={handleClick}>search</button>
             </div>
             <div>
                 <ul>
